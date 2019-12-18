17058e320162c444b0bcd98a064d8f57b977b965
Andrew Hartill <hartilla@gmail.com>
2019-12-18 16:25:20 +1100

    Update Utils.cs

--- AzureFunctionForSplunk/Utils.cs
+++ AzureFunctionForSplunk/Utils.cs
@@ -251,7 +251,7 @@ namespace AzureFunctionForSplunk
             }
             catch (System.Net.Http.HttpRequestException e)
             {
-                throw new System.Net.Http.HttpRequestException("Sending to Proxy Function. Is the service running?", e.Message);
+                throw new System.Net.Http.HttpRequestException("Sending to Proxy Function. Is the service running?", e);
             }
             catch (Exception f)
             {
@@ -304,7 +304,7 @@ namespace AzureFunctionForSplunk
                 }
                 catch (System.Net.Http.HttpRequestException e)
                 {
-                    throw new System.Net.Http.HttpRequestException("Sending to Splunk. Is Splunk service running?", e);
+                    throw new System.Net.Http.HttpRequestException("Sending to Splunk. Is Splunk service running?", e.Message);
                 }
                 catch (Exception f)
                 {
